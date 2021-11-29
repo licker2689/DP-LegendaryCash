7c859b87bbd3ad387fb85a0f4f66316a615eec9c
darksoldier1404 <darksoldier1404@gmail.com>
2021-11-30 04:03:57 +0900

    DP-LegendaryCash-v1.0.1.52-_1.12.2

---
+++ v1_12_2/src/main/java/com/darksoldier1404/dlc/LegendaryCash.java
@@ -0,0 +1,54 @@
+package com.darksoldier1404.dlc;
+
+import com.darksoldier1404.dlc.commands.CashCommand;
+import com.darksoldier1404.dlc.commands.CashShopCommand;
+import com.darksoldier1404.dlc.events.DLCEvent;
+import com.darksoldier1404.dlc.utils.ShopConfigUtil;
+import com.darksoldier1404.dlc.utils.Utils;
+import com.darksoldier1404.duc.UniversalCore;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class LegendaryCash extends JavaPlugin {
+    private UniversalCore core;
+    private static LegendaryCash plugin;
+    public String prefix;
+    public YamlConfiguration config;
+    public Map<UUID, YamlConfiguration> udata = new HashMap<>();
+    public Map<String, YamlConfiguration> shops = new HashMap<>();
+    public Map<UUID, String> currentEditShop = new HashMap<>();
+
+    public static LegendaryCash getInstance() {
+        return plugin;
+    }
+
+
+    public void onEnable() {
+        plugin = this;
+        Plugin pl = getServer().getPluginManager().getPlugin("DP-UniversalCore");
+        if(pl == null) {
+            getLogger().warning("DP-UniversalCore 플러그인이 설치되어있지 않습니다.");
+            getLogger().warning("DP-LegendaryCash 플러그인을 비활성화 합니다.");
+            plugin.setEnabled(false);
+            return;
+        }
+        core = (UniversalCore) pl;
+        Utils.loadDefaultConfig();
+        ShopConfigUtil.loadAllShop();
+        plugin.getServer().getPluginManager().registerEvents(new DLCEvent(), plugin);
+        getCommand("캐시").setExecutor(new CashCommand());
+        getCommand("캐시상점").setExecutor(new CashShopCommand());
+    }
+
+    public void onDisable() {
+        for (UUID uuid : udata.keySet()) {
+            Utils.saveData(uuid);
+        }
+        ShopConfigUtil.saveAllShop();
+    }
+}
---
+++ v1_12_2/src/main/java/com/darksoldier1404/dlc/events/DLCEvent.java
@@ -0,0 +1,82 @@
+package com.darksoldier1404.dlc.events;
+
+import com.darksoldier1404.dlc.LegendaryCash;
+import com.darksoldier1404.dlc.functions.CashFunction;
+import com.darksoldier1404.dlc.functions.CashShopFunction;
+import com.darksoldier1404.dlc.utils.Utils;
+import com.darksoldier1404.duc.utils.NBT;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+@SuppressWarnings("all")
+public class DLCEvent implements Listener {
+    private final LegendaryCash plugin = LegendaryCash.getInstance();
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        Utils.initData(p.getUniqueId());
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        Utils.quitAndSaveData(p.getUniqueId());
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        Player p = (Player) e.getPlayer();
+        if (e.getView().getTitle().contains("캐시상점 진열")) {
+            CashShopFunction.saveShopShowCase(p, e.getView().getTopInventory());
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        if (e.getView().getTitle().contains("캐시상점 아이템 목록")) {
+            e.setCancelled(true);
+            if (e.getClick() == ClickType.LEFT) {
+                CashShopFunction.buyWithCash(p, e.getCurrentItem());
+                return;
+            }
+            if (e.getClick() == ClickType.RIGHT) {
+                CashShopFunction.buyWithMileage(p, e.getCurrentItem());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInteractCheck(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if(e.getHand() == EquipmentSlot.OFF_HAND) return; // 더블클릭 방지
+        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if(e.getItem() == null) return;
+            ItemStack item = e.getItem();
+            if(e.getItem().getItemMeta() == null) return;
+            if(NBT.hasTagKey(e.getItem(), "CASH")) {
+                double cash = Double.parseDouble(NBT.getStringTag(e.getItem(), "CASH").replace('"', ' ').trim());
+                CashFunction.addCash(p, cash);
+                item.setAmount(item.getAmount() - 1);
+                p.sendMessage(plugin.prefix + "§e" + cash + "§a캐시를 획득했습니다.");
+            }
+            else if(NBT.hasTagKey(e.getItem(), "MILEAGE")) {
+                double mileage = Double.parseDouble(NBT.getStringTag(e.getItem(), "MILEAGE").replace('"', ' ').trim());
+                CashFunction.addMileage(p, mileage);
+                item.setAmount(item.getAmount() - 1);
+                p.sendMessage(plugin.prefix + "§e" + mileage + "§a마일리지를 획득했습니다.");
+            }
+        }
+    }
+}
