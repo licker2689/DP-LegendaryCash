722f1822472ba603c752b12b9f3b33ed4766a3f9
darksoldier1404 <darksoldier1404@gmail.com>
2022-06-14 20:47:31 +0900

    1.19 업데이트, 1.12.2 모듈 부활

--- src/main/java/com/darksoldier1404/dlc/events/DLCEvent.java
+++ src/main/java/com/darksoldier1404/dlc/events/DLCEvent.java
@@ -45,11 +45,11 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onChat(PlayerChatEvent e) {
         String msg = e.getMessage();
-        if(msg.contains("{cash}")) {
-            e.setMessage(e.getMessage().replace("{cash}", ((HashMap<UUID, Double>)plugin.dphm.getPlaceholder("cash").getValue()).get(e.getPlayer().getUniqueId())+""));
+        if (msg.contains("{cash}")) {
+            e.setMessage(e.getMessage().replace("{cash}", ((HashMap<UUID, Double>) plugin.dphm.getPlaceholder("cash").getValue()).get(e.getPlayer().getUniqueId()) + ""));
         }
-        if(msg.contains("{mileage}")) {
-            e.setMessage(e.getMessage().replace("{mileage}", ((HashMap<UUID, Double>)plugin.dphm.getPlaceholder("mileage").getValue()).get(e.getPlayer().getUniqueId())+""));
+        if (msg.contains("{mileage}")) {
+            e.setMessage(e.getMessage().replace("{mileage}", ((HashMap<UUID, Double>) plugin.dphm.getPlaceholder("mileage").getValue()).get(e.getPlayer().getUniqueId()) + ""));
         }
     }
 
@@ -57,9 +57,9 @@ public class DLCEvent implements Listener {
     public void onInventoryClose(InventoryCloseEvent e) {
         Player p = (Player) e.getPlayer();
         if(e.getInventory() instanceof DInventory) {
-            if(!plugin.currentEditShop.containsKey(p.getUniqueId())) return;
+            if (!plugin.currentEditShop.containsKey(p.getUniqueId())) return;
             DInventory di = (DInventory) e.getInventory();
-            if(di.isValidHandler(plugin)) {
+            if (di.isValidHandler(plugin)) {
                 if (e.getView().getTitle().equals(lang.getWithArgs("shop_display_gui_title", plugin.currentEditShop.get(p.getUniqueId())))) {
                     CashShopFunction.saveShopShowCase(p, (Inventory) di);
                 }
@@ -70,11 +70,11 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
         Player p = (Player) e.getWhoClicked();
-        if(e.getClickedInventory() == null) return;
-        if(e.getClickedInventory() instanceof DInventory) {
-            if(plugin.currentEditShop.containsKey(e.getWhoClicked().getUniqueId())) return;
+        if (e.getClickedInventory() == null) return;
+        if (e.getClickedInventory() instanceof DInventory) {
+            if (plugin.currentEditShop.containsKey(e.getWhoClicked().getUniqueId())) return;
             DInventory di = (DInventory) e.getClickedInventory();
-            if(di.isValidHandler(plugin)) {
+            if (di.isValidHandler(plugin)) {
                 e.setCancelled(true);
                 if (e.getClick() == ClickType.LEFT) {
                     CashShopFunction.buyWithCash(p, e.getCurrentItem());
@@ -90,18 +90,17 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onInteractCheck(PlayerInteractEvent e) {
         Player p = e.getPlayer();
-        if(e.getHand() == EquipmentSlot.OFF_HAND) return; // 더블클릭 방지
-        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if(e.getItem() == null) return;
+        if (e.getHand() == EquipmentSlot.OFF_HAND) return; // 더블클릭 방지
+        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem() == null) return;
             ItemStack item = e.getItem();
-            if(e.getItem().getItemMeta() == null) return;
-            if(NBT.hasTagKey(e.getItem(), "CASH")) {
+            if (e.getItem().getItemMeta() == null) return;
+            if (NBT.hasTagKey(e.getItem(), "CASH")) {
                 double cash = NBT.getDoubleTag(e.getItem(), "CASH");
                 CashFunction.addCash(p, cash);
                 item.setAmount(item.getAmount() - 1);
                 p.sendMessage(plugin.prefix + "§e" + cash + "§a캐시를 획득했습니다.");
-            }
-            else if(NBT.hasTagKey(e.getItem(), "MILEAGE")) {
+            } else if (NBT.hasTagKey(e.getItem(), "MILEAGE")) {
                 double mileage = NBT.getDoubleTag(e.getItem(), "MILEAGE");
                 CashFunction.addMileage(p, mileage);
                 item.setAmount(item.getAmount() - 1);
--- v1_12_2/src/main/java/com/darksoldier1404/dlc/events/DLCEvent.java
+++ v1_12_2/src/main/java/com/darksoldier1404/dlc/events/DLCEvent.java
@@ -44,23 +44,27 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onChat(PlayerChatEvent e) {
         String msg = e.getMessage();
-        if(msg.contains("{cash}")) {
-            e.setMessage(e.getMessage().replace("{cash}", ((HashMap<UUID, Double>)plugin.dphm.getPlaceholder("cash").getValue()).get(e.getPlayer().getUniqueId())+""));
+        if (msg.contains("{cash}")) {
+            e.setMessage(e.getMessage().replace("{cash}", ((HashMap<UUID, Double>) plugin.dphm.getPlaceholder("cash").getValue()).get(e.getPlayer().getUniqueId()) + ""));
         }
-        if(msg.contains("{mileage}")) {
-            e.setMessage(e.getMessage().replace("{mileage}", ((HashMap<UUID, Double>)plugin.dphm.getPlaceholder("mileage").getValue()).get(e.getPlayer().getUniqueId())+""));
+        if (msg.contains("{mileage}")) {
+            e.setMessage(e.getMessage().replace("{mileage}", ((HashMap<UUID, Double>) plugin.dphm.getPlaceholder("mileage").getValue()).get(e.getPlayer().getUniqueId()) + ""));
         }
     }
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent e) {
         Player p = (Player) e.getPlayer();
-        if(e.getInventory() instanceof DInventory) {
-            if(!plugin.currentEditShop.containsKey(p.getUniqueId())) return;
-            DInventory di = (DInventory) e.getInventory();
-            if(di.isValidHandler(plugin)) {
-                if (e.getView().getTitle().equals(lang.getWithArgs("shop_display_gui_title", plugin.currentEditShop.get(p.getUniqueId())))) {
-                    CashShopFunction.saveShopShowCase(p, di);
+        if (!plugin.currentEditShop.containsKey(p.getUniqueId())) {
+            CashShopFunction.currentInv.remove(p.getUniqueId());
+        } else {
+            if (CashShopFunction.currentInv.containsKey(p.getUniqueId())) {
+                DInventory di = CashShopFunction.currentInv.get(p.getUniqueId());
+                if (di.isValidHandler(plugin)) {
+                    if (e.getView().getTitle().equals(lang.getWithArgs("shop_display_gui_title", plugin.currentEditShop.get(p.getUniqueId())))) {
+                        CashShopFunction.saveShopShowCase(p, di);
+                        CashShopFunction.currentInv.remove(p.getUniqueId());
+                    }
                 }
             }
         }
@@ -69,11 +73,11 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
         Player p = (Player) e.getWhoClicked();
-        if(e.getInventory() == null) return;
-        if(e.getInventory() instanceof DInventory) {
-            if(plugin.currentEditShop.containsKey(e.getWhoClicked().getUniqueId())) return;
-            DInventory di = (DInventory) e.getInventory();
-            if(di.isValidHandler(plugin)) {
+        if (e.getInventory() == null) return;
+        if (plugin.currentEditShop.containsKey(e.getWhoClicked().getUniqueId())) return;
+        if (CashShopFunction.currentInv.containsKey(p.getUniqueId())) {
+            DInventory di = CashShopFunction.currentInv.get(p.getUniqueId());
+            if (di.isValidHandler(plugin)) {
                 e.setCancelled(true);
                 if (e.getClick() == ClickType.LEFT) {
                     CashShopFunction.buyWithCash(p, e.getCurrentItem());
@@ -89,18 +93,17 @@ public class DLCEvent implements Listener {
     @EventHandler
     public void onInteractCheck(PlayerInteractEvent e) {
         Player p = e.getPlayer();
-        if(e.getHand() == EquipmentSlot.OFF_HAND) return; // 더블클릭 방지
-        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if(e.getItem() == null) return;
+        if (e.getHand() == EquipmentSlot.OFF_HAND) return; // 더블클릭 방지
+        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (e.getItem() == null) return;
             ItemStack item = e.getItem();
-            if(e.getItem().getItemMeta() == null) return;
-            if(NBT.hasTagKey(e.getItem(), "CASH")) {
+            if (e.getItem().getItemMeta() == null) return;
+            if (NBT.hasTagKey(e.getItem(), "CASH")) {
                 double cash = NBT.getDoubleTag(e.getItem(), "CASH");
                 CashFunction.addCash(p, cash);
                 item.setAmount(item.getAmount() - 1);
                 p.sendMessage(plugin.prefix + "§e" + cash + "§a캐시를 획득했습니다.");
-            }
-            else if(NBT.hasTagKey(e.getItem(), "MILEAGE")) {
+            } else if (NBT.hasTagKey(e.getItem(), "MILEAGE")) {
                 double mileage = NBT.getDoubleTag(e.getItem(), "MILEAGE");
                 CashFunction.addMileage(p, mileage);
                 item.setAmount(item.getAmount() - 1);
